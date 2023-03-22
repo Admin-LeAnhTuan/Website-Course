3d6b1321702276d7560534e951601da943d47509
Danh NC <nguyencongdanh2002@gmail.com>
2023-03-22 23:54:55 +0700

    Add Controller

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Course.Models;
+
+namespace Course.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private ModelContext db = new ModelContext();
+
+        // GET: Courses
+        public ActionResult Index()
+        {
+            var courses = db.Courses.Include(c => c.AspNetUser).Include(c => c.Category);
+            return View(courses.ToList());
+        }
+
+        // GET: Courses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Courses courses = db.Courses.Find(id);
+            if (courses == null)
+            {
+                return HttpNotFound();
+            }
+            return View(courses);
+        }
+
+        // GET: Courses/Create
+        public ActionResult Create()
+        {
+            ViewBag.userid = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.category_id = new SelectList(db.Categories, "category_id", "Name");
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "course_id,title,description,price,duration,userid,category_id")] Courses courses)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(courses);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userid = new SelectList(db.AspNetUsers, "Id", "Email", courses.userid);
+            ViewBag.category_id = new SelectList(db.Categories, "category_id", "Name", courses.category_id);
+            return View(courses);
+        }
+
+        // GET: Courses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Courses courses = db.Courses.Find(id);
+            if (courses == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userid = new SelectList(db.AspNetUsers, "Id", "Email", courses.userid);
+            ViewBag.category_id = new SelectList(db.Categories, "category_id", "Name", courses.category_id);
+            return View(courses);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "course_id,title,description,price,duration,userid,category_id")] Courses courses)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(courses).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userid = new SelectList(db.AspNetUsers, "Id", "Email", courses.userid);
+            ViewBag.category_id = new SelectList(db.Categories, "category_id", "Name", courses.category_id);
+            return View(courses);
+        }
+
+        // GET: Courses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Courses courses = db.Courses.Find(id);
+            if (courses == null)
+            {
+                return HttpNotFound();
+            }
+            return View(courses);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Courses courses = db.Courses.Find(id);
+            db.Courses.Remove(courses);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
