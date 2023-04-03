cca580eb0131237201f008e871806359fd8cb2a2
Anh Tuan <leanhtuan.svhutech@gmail.com>
2023-04-03 17:11:12 +0700

    add edit

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,9 +1,6 @@
 using Course.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +11,7 @@ namespace Course
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
